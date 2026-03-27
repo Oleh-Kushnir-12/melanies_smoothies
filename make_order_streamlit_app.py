ffd6567da48e88eddb8ee691203cbc2300b6b7f6
Oleh-Kushnir-12 <131269558+Oleh-Kushnir-12@users.noreply.github.com>
2026-03-27 17:08:55 +0200

    Update selected columns in fruit_options query

--- make_order_streamlit_app.py
+++ make_order_streamlit_app.py
@@ -12,7 +12,7 @@ st.write("The current name of your Smootie is ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
